cd257939a551e5c661a55063fd984e80708a19c5
wojta <vojtech.wrnata@tul.cz>
2014-12-17 16:07:10 +0100

    prvni commit v nemocnici

---
+++ src/cz/tul/cc/mastercurve/MasterCurveInterface.java
@@ -0,0 +1,10 @@
+package cz.tul.cc.mastercurve;
+
+/** ************************************************************************
+ * Basic interface for Master Curve Genetaror
+ *
+ * @author wojta
+ */
+public interface MasterCurveInterface {
+    
+}
