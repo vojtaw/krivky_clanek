eae4dfebb7e5bc89454122a0e1e13a1c2f08b055
wojta <vojtech.wrnata@tul.cz>
2014-12-17 16:05:55 +0100

    prvni commit v nemocnici

--- src/cz/tul/cc/discretization/DiscretizationTest.java
+++ src/cz/tul/cc/discretization/DiscretizationTest.java
@@ -15,7 +15,7 @@ public class DiscretizationTest {
     private static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(DiscretizationTest.class.getName());
     private int numberOfSteps;
     /** ********************************************************************
-     * Testing line starting at[0.0; 0.0] and ending at [1.0; 0.0]
+     * Testing line starting at[0.0; 0.0] and ending at [1.0; 1.0]
      * so lenght of line is exactly 1
      */
     private final Point startPoint = new Point(0.0, 0.0);
@@ -42,15 +42,17 @@ public class DiscretizationTest {
     /** public methods ***************************************************** */
     /** ******************************************************************** */
     public void test(){
-        for (int i=2000; i<=numberOfSteps; i++){
-//            result.put(i, _getLength(i));
+        for (int i=1; i<=numberOfSteps; i++){
+            result.put(i, _getLength(i));
             
-            System.out.println("Length for " + i + " discretization: " + (_getLength(i)-1.0));
-            for (Point p : discretizationPoints) {
-                System.out.println(p.getX());
-            }
+//            System.out.println("Difference for " + i + " discretization: " + (_getLength(i)-Math.sqrt(2)));
+//            for (Point p : discretizationPoints) {
+//                System.out.println(p.getX());
+//            }
         }
-        
+        for (Integer i : result.keySet()) {
+                System.out.println(result.get(i)-Math.sqrt(2));
+            }
     }
     /** ******************************************************************** */
     /** private methods **************************************************** */
@@ -71,7 +73,7 @@ public class DiscretizationTest {
         double step = 1.0/parts;
         for(int i=2; i<=parts; i++){
             next = previous+step;
-            discretizationPoints.add(new Point(next, 0));
+            discretizationPoints.add(new Point(next, next));
             previous=next;
         }
         discretizationPoints.add(endPoint);
@@ -80,9 +82,12 @@ public class DiscretizationTest {
     private double _calculateLength(){
         double length = 0.0;
         Point prev = discretizationPoints.get(0);
-        
+        double dx=0, dy=0;
         for(int i=1; i<discretizationPoints.size(); i++){
-            length += (discretizationPoints.get(i).getX() - prev.getX());
+            dx=0; dy=0;
+            dx=discretizationPoints.get(i).getX() - prev.getX();
+            dy=discretizationPoints.get(i).getY() - prev.getY();
+            length += Math.sqrt(dx*dx + dy*dy);
             prev = discretizationPoints.get(i);
         }
         return length;
